e5455e0582b407feb25380f1010f719e73e63a55
León Cantillo <leonjob13@gmail.com>
2025-01-23 11:18:09 -0500

    [UPDATE] testing database.

--- src/modules/clients/routes.js
+++ src/modules/clients/routes.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import answers from '../../net/answers.js';
+import controller from './controller.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    answers.success(req, res, 'Todo Ok desde clientes', 200);
+router.get('/', async (req, res) => {
+    await controller.all()
+        .then((items) => {
+            answers.success(req, res, items, 200);
+        })
+        .catch((err) => {
+            console.error('[Error getting the database]:', err);
+            answers.error(req, res, null, 500);
+        });;
 });
 
 export default router;
