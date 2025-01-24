867a6951a75a4b3fef7cd499ca85b4fc54a31e4f
León Cantillo <leonjob13@gmail.com>
2025-01-24 14:25:10 -0500

    [UPDATE] adapting to new routes and error handling.

--- src/modules/clients/routes.js
+++ src/modules/clients/routes.js
@@ -4,15 +4,49 @@ import controller from './controller.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', all);
+router.get('/:id', one);
+router.post('/', create);
+router.put('/', remove);
+
+async function all(req, res, next) {
     await controller.all()
         .then((items) => {
             answers.success(req, res, items, 200);
         })
         .catch((err) => {
-            console.error('[Error getting the database]:', err);
-            answers.error(req, res, null, 500);
-        });;
-});
+            next(err);
+        });
+}
+
+async function one(req, res, next) {
+    await controller.one(req.params.id)
+        .then((item) => {
+            answers.success(req, res, item, 200);
+        })
+        .catch((err) => {
+            next(err);
+        });
+}
+
+async function create(req, res, next) {
+    await controller.create(req.body)
+        .then(() => {
+            answers.success(req, res, 'Item Successfully Created', 200);
+        })
+        .catch((err) => {
+            next(err);
+        });
+}
+
+async function remove(req, res, next) {
+    await controller.remove(req.body)
+        .then(() => {
+            answers.success(req, res, 'Satisfactory removal', 200);
+        })
+        .catch((err) => {
+            next(err);
+        });
+}
 
 export default router;
