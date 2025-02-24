27ebf7ee4138e4b3052df1706ec78df40998e8bc
León Cantillo <leonjob13@gmail.com>
2025-02-23 23:04:54 -0500

    [ADD] User data logic. Controller and routes-.

---
+++ src/modules/users/index.js
@@ -0,0 +1,4 @@
+import db from '../../db/database.js';
+import controller from './controller.js';
+
+export default controller(db);
---
+++ src/modules/users/routes.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import answers from '../../net/answers.js';
+import controller from './index.js';
+
+const router = express.Router();
+
+router.get('/', all);
+router.get('/:id', one);
+router.post('/', create);
+router.put('/', remove);
+
+async function all(req, res, next) {
+    await controller.all()
+        .then((items) => {
+            answers.success(req, res, items, 200);
+        })
+        .catch((err) => {
+            next(err);
+        });
+}
+
+async function one(req, res, next) {
+    await controller.one(req.params.id)
+        .then((item) => {
+            answers.success(req, res, item, 200);
+        })
+        .catch((err) => {
+            next(err);
+        });
+}
+
+async function create(req, res, next) {
+    await controller.create(req.body)
+        .then((item) => {
+            answers.success(req, res, item, 200);
+        })
+        .catch((err) => {
+            next(err);
+        });
+}
+
+async function remove(req, res, next) {
+    await controller.remove(req.body)
+        .then(() => {
+            answers.success(req, res, 'Satisfactory removal', 200);
+        })
+        .catch((err) => {
+            next(err);
+        });
+}
+
+export default router;
